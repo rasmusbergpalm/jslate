5caa1c06d1ec7a9d58ea919f88ade4d0e1a5500b
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2012-06-19 18:27:07 +0200

    session lifetime, twitter and clock widget

--- app/config/core.php
+++ app/config/core.php
@@ -170,7 +170,7 @@
  * Session time out time (in seconds).
  * Actual value depends on 'Security.level' setting.
  */
-	Configure::write('Session.timeout', '120');
+	Configure::write('Session.timeout', 2592000);
 
 /**
  * If set to false, sessions are not automatically started.
