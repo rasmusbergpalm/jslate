5f812dc44b5db7cb37598c6d1e3c3a480922a011
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-07-01 23:52:08 +0200

    remove fixture data

--- app/Test/Fixture/DashboardFixture.php
+++ app/Test/Fixture/DashboardFixture.php
@@ -5,23 +5,4 @@
  */
 class DashboardFixture extends CakeTestFixture {
     public $import = array('table' => 'dashboards');
-
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'name' => 'Lorem ipsum dolor sit amet',
-			'user_id' => 1
-		),
-        array(
-            'id' => 2,
-            'name' => 'Lorem ipsum dolor sit amet',
-            'user_id' => 1
-        ),
-	);
-
 }
--- app/Test/Fixture/UserFixture.php
+++ app/Test/Fixture/UserFixture.php
@@ -5,18 +5,4 @@
  */
 class UserFixture extends CakeTestFixture {
     public $import = array('table' => 'users');
-
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'email' => 'test@test.com',
-			'password' => 'test'
-		),
-	);
-
 }
