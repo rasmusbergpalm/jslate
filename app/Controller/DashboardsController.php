c7e31d36d8b35887df757c28e2213193380133eb
Brian Hawkins <brianhks1@gmail.com>
2013-08-13 12:20:09 -0600

    Changed code to be more readable

--- app/Controller/DashboardsController.php
+++ app/Controller/DashboardsController.php
@@ -44,16 +44,17 @@ class DashboardsController extends AppController {
         if (!empty($this->request->data)) {
             $upload = $this->request->data['Dashboard']['import'];
             
-            if (!empty($upload['tmp_name'])) {
-                $json = json_decode(file_get_contents($upload['tmp_name']), true);
-                $json['Dashboard']['user_id'] = $this->Auth->user('id');
-                $json['Dashboard']['public_id'] = Security::hash(openssl_random_pseudo_bytes(40));
-                if ($this->Dashboard->saveAssociated($json)) {
-                    return $this->redirect(array('action' => 'view', $this->Dashboard->getLastInsertId()));
-                }
-                else {
-                    $this->Session->setFlash(__('You must specify a valid jslate export.'), 'error');
-                }
+            if (empty($upload['tmp_name'])) {
+                $this->Session->setFlash(__('You must specify a valid jslate export.'), 'error');
+                return;
+            }
+            
+            $json = json_decode(file_get_contents($upload['tmp_name']), true);
+            $json['Dashboard']['user_id'] = $this->Auth->user('id');
+            $json['Dashboard']['public_id'] = Security::hash(openssl_random_pseudo_bytes(40));
+            
+            if ($this->Dashboard->saveAssociated($json)) {
+                return $this->redirect(array('action' => 'view', $this->Dashboard->getLastInsertId()));
             }
             else {
                 $this->Session->setFlash(__('You must specify a valid jslate export.'), 'error');
